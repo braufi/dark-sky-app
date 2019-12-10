771616820159e26834be1517c14911a62f4ba7e6
braufi <39024381+braufi@users.noreply.github.com>
2019-12-10 13:50:59 +0100

    Final

---
+++ react-todo-app/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+import Todos from './components/Todos';
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash.',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife.',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Business meeting.',
+        completed: false
+      }
+    ]
+  }
+  render() {
+    console.log(this.state.Todos);
+    return (
+      <div className="App">
+        <Todos />
+      </div>
+    );
+  }
+}
+
+export default App;
